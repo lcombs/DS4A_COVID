910ec5f749a48221805dd49ca77b8ac5fb7cf814
Lisa Combs <eac721@nyu.edu>
2020-10-03 15:46:43 -0400

    master_model file

---
+++ model_master.R
@@ -0,0 +1,15 @@
+library(panelvar)
+
+path<-'/Users/lisacombs/Documents/DS4A/Project/all_data_clean.csv'
+
+data <-read.csv(path)
+head(data)
+
+###### Cathy ###### 
+
+###### Jingjing ###### 
+
+###### Lencer ###### 
+
+###### Lisa ###### 
+
